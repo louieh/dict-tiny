dfa70c5f40ca9e2be2bc2365ad826fbbea0fd70f
louieh <louiehan1015@gmail.com>
2018-06-22 00:23:16 +0800

    cancel switch -c; automatic judgment in English and Chinese; add -m to get more detail translation; add -c to translate the content of clipboard

--- dict_tiny/main.py
+++ dict_tiny/main.py
@@ -7,6 +7,7 @@ from lxml import html
 import re
 import argparse
 from plumbum import cli
+import pyperclip
 
 from en_detail.get_detail import get_data, print_basetrans, print_detailtrans
 
@@ -21,7 +22,7 @@ class Dict_tiny(cli.Application):
     DESCRIPTION = "A tiny command-line dictionary that scrapes youdao.com. Just for fun."
 
     # verbose = cli.Flag(["v", "verbose"], help="If given, I will be very talkative")
-
+    # more = cli.switch(["m","more"], help="Getting more detial.")
     IS_TRANS = 0
 
     FAKE_HEADER = {
@@ -38,12 +39,6 @@ class Dict_tiny(cli.Application):
         """
         self.IS_TRANS = 1
 
-        if self.is_alphabet(word) != 'en':
-            print("[Error!] You should enter an English word.")
-            print("------------------")
-            self.help()
-            return
-
         data = self.downloader(word)
         content = data.xpath('.//div[@id="phrsListTab"]/div[@class="trans-container"]/ul/li//text()')
         print(word)
@@ -55,13 +50,11 @@ class Dict_tiny(cli.Application):
                 print(each_result)
             return
         else:
-            print("None.")
-            # print("------------------")
-            # self.help()
+            print("No translation found for this word.")
             return
 
-    @cli.switch(["-m", "--more"],str)
-    def trans_en_more(self, word):
+    @cli.switch(["-m", "--more"], str)
+    def trans_en_more(self, word, row=3, printall=True):
         """
         English_Chinese_detail
         """
@@ -69,26 +62,18 @@ class Dict_tiny(cli.Application):
 
         if self.is_alphabet(word) != 'en':
             print("[Error!] You should enter an English word.")
-            print("------------------")
-            self.help()
             return
 
         data_base = get_data(word)
         print_basetrans(data_base)
-        print_detailtrans(data_base)
+        print_detailtrans(data_base, row, printall)  # two parameters：row=3, printall=False
+        return
 
-    @cli.switch(["-c", "--chinese"], str)
     def trans_cn(self, word):
         """
         Chinese_English
         """
         self.IS_TRANS = 1
-
-        if self.is_alphabet(word) != 'cn':
-            print("[Error!] You should enter a Chinese word.")
-            print("------------------")
-            self.help()
-            return
 
         data = self.downloader(word)
         content = data.xpath('.//div[@id="phrsListTab"]/div[@class="trans-container"]/ul//span//text()')
@@ -107,12 +92,28 @@ class Dict_tiny(cli.Application):
             print(content)
             return
         else:
-            print("None.")
-            # print("------------------")
-            # self.help()
+            print("No translation found for this word.")
             return
-        # print("This is translation_cn.")
-        # print("You want to translate '%s' to English" % word)
+
+    @cli.switch(["-c", "--clipboard"])
+    def trans_clipboard(self):
+        """
+        Translate the content of the current clipboard if it`s an English word or a Chinese word.
+        No parameter required.
+        """
+        self.IS_TRANS = 1
+
+        try:
+            clipboard_data = pyperclip.paste().strip().replace('\n', '')
+        except:
+            print("[Error!] Cannot get clipboard content.")
+            return
+
+        if clipboard_data:
+            self.main_(clipboard_data)
+        else:
+            print("There is no content in the clipboard.")
+        return
 
     def downloader(self, word):
         request_url = "http://youdao.com/w/%s" % word
@@ -121,14 +122,34 @@ class Dict_tiny(cli.Application):
         return result_selector
 
     def is_alphabet(self, word):
-        if word >= '\u4e00' and word <= '\u9fff':
+        is_alphabet = {
+            'cn': 0,
+            'en': 0,
+        }
+        for each_letter in word:
+            if each_letter >= '\u4e00' and each_letter <= '\u9fff':
+                is_alphabet['cn'] += 1
+            # elif word >= '\u0030' and word <= '\u0039':
+            #     return 'num'
+            elif (each_letter >= '\u0041' and each_letter <= '\u005a') or (
+                    each_letter >= '\u0061' and each_letter <= '\u007a'):
+                is_alphabet['en'] += 1
+
+        if is_alphabet['cn'] == len(word):
             return 'cn'
-        # elif word >= '\u0030' and word <= '\u0039':
-        #     return 'num'
-        elif (word >= '\u0041' and word <= '\u005a') or (word >= '\u0061' and word <= '\u007a'):
+        elif is_alphabet['en'] == len(word):
             return 'en'
         else:
             return 'other'
+
+    def main_(self, word):
+        if self.is_alphabet(word) == 'en':
+            self.trans_en(word)
+        elif self.is_alphabet(word) == 'cn':
+            self.trans_cn(word)
+        else:
+            print("[Error!] This is not an English word or a Chinese word.")
+        return
 
     def main(self, *word):
         if not word and not self.IS_TRANS:
@@ -137,8 +158,8 @@ class Dict_tiny(cli.Application):
             if len(word) > 1:
                 print(
                     "Oops! You may want to translate a sentence, but I can only choose the first word as the target word.")
-            self.trans_en(word[0])
-        # print("This is main function.")
+                word = word[0]
+                self.main_(word)
         # if self.verbose:
         #     print("")
         return
