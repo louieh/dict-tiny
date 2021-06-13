9889d54a9f3c5d630df9f5f6d502744c5268b724
louieh <louiehan1015@gmail.com>
2021-06-13 00:26:52 -0500

    v0.3.3 -> v1.0

--- dict_tiny/version.py
+++ dict_tiny/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 name = "dict-tiny"
-__version__ = '0.3.3'
+__version__ = '1.0'
 DESCRIPTION = "A tiny command-line dictionary and translator implemented in Plumbum. Youdao.com and Google Translation API. Just for fun :)"
