85d113cde5fce79bfbff44e2bd0ef8b1b6c2e1ee
louieh <louiehan1015@gmail.com>
2021-11-19 01:46:49 +0800

    update version and describtion

--- dict_tiny/version.py
+++ dict_tiny/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 name = "dict-tiny"
-__version__ = '1.1.0'
-DESCRIPTION = "A tiny command-line dictionary and translator implemented in Plumbum. Google Translate and DeepL Translator and Youdao. Just for fun :)"
+__version__ = '1.1.1'
+DESCRIPTION = "A command-line translator was implemented in Plumbum. Google Translate and DeepL Translator and Youdao. Just for fun :)"
