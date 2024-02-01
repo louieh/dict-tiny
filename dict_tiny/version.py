c18f0b09f4731c488c9c2f1514d1d7fe0c108c82
louieh <louiehan1015@gmail.com>
2024-02-01 14:14:46 +0800

    update version to 1.3.0a1

--- dict_tiny/version.py
+++ dict_tiny/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 name = "dict-tiny"
-__version__ = '1.2.1'
+__version__ = '1.3.0a1'
 DESCRIPTION = "A command-line tool that integrates Youdao Dict, Google Translate, Gemini and OpenAI. Just for fun :)"
