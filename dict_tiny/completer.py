dfb5e8591b08c8aa5dbb56584262f21810c6a208
louieh <louiehan1015@gmail.com>
2026-05-21 15:07:52 +0800

    update: 增加自动补全中explain内容

--- dict_tiny/completer.py
+++ dict_tiny/completer.py
@@ -1,6 +1,10 @@
 from prompt_toolkit.completion import Completer, Completion
 
-from dict_tiny.config import YOUDAO_SUGGESTION_API_BASE_URL, SUGGESTION_NUM, YOUDAO_SUGGEST_API_FAKE_HEADER
+from dict_tiny.config import (
+    YOUDAO_SUGGESTION_API_BASE_URL,
+    SUGGESTION_NUM,
+    YOUDAO_SUGGEST_API_FAKE_HEADER,
+)
 from dict_tiny.util import downloader
 
 
@@ -10,15 +14,22 @@ class YoudaoCompleter(Completer):
 
     def get_completions(self, document, complete_event):
         word_before_cursor = document.get_word_before_cursor()
-        suggest_url = YOUDAO_SUGGESTION_API_BASE_URL.format(SUGGESTION_NUM, self.le, word_before_cursor)
-        resp = downloader.download("GET", suggest_url, headers=YOUDAO_SUGGEST_API_FAKE_HEADER)
-        if not resp: return []
+        suggest_url = YOUDAO_SUGGESTION_API_BASE_URL.format(
+            SUGGESTION_NUM, self.le, word_before_cursor
+        )
+        resp = downloader.get(suggest_url, headers=YOUDAO_SUGGEST_API_FAKE_HEADER)
+        if not resp:
+            return []
         try:
             resp_json = resp.json()
             if resp_json["result"]["code"] != 200:
                 return []
             for each in resp_json["data"]["entries"]:
                 entry, explain = each["entry"], each["explain"]
-                yield Completion(entry, start_position=-len(word_before_cursor))
+                yield Completion(
+                    entry,
+                    start_position=-len(word_before_cursor),
+                    display_meta=explain[:10],
+                )
         except Exception as e:
             return []
